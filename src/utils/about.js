987a150314bad22ef808d534e6a3524c2fadaea4
naftalib <45719650+naftalib@users.noreply.github.com>
2021-01-07 14:23:58 +0200

    Updates

--- src/utils/about.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const about = `Hi, I'm Natfali, A web developer and multimedia designer, rich in ideas to design and develop all purpose modern web applications. Highly motivated and passionate about problem solving, always trying to find the most logical yet creative solution to any challenge.`
-
-export default about
