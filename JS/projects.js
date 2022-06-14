ad1a7a4f3546a68cef84f851654c4efedb58c406
naftalib <45719650+naftalib@users.noreply.github.com>
2022-06-15 02:49:11 +0300

    generic icons

--- JS/projects.js
+++ JS/projects.js
@@ -1,13 +1,13 @@
 const projects = {
     one:{
-        img: "./assets/DATA/imgs/dashboard.PNG",
+        img: "./assets/DATA/imgs/js.PNG",
         title: 'Personal Dashboard app',
         description: `This little app is a cool chrome extention based off of the momentum browser extention. I gave it a few other features like a currency converter.`,	
         code: 'https://github.com/naftalib/dashBoard/blob/main/index.js',
         link: 'https://github.com/naftalib/dashBoard'
     },
     two:{
-        img:"./assets/DATA/imgs/robocards.JPG",
+        img:"./assets/DATA/imgs/react.PNG",
         title: 'Robo Cards',
         description: `This React app displays character cards, generated with a http-call to robohash.org to retrieve character figures and the JSON placeholder/users API. The characters are searchable according to name. I plan on adding some more features to it soon`,
 		code: 'https://github.com/naftalib/roboQuotes/tree/main',
@@ -22,7 +22,7 @@ const projects = {
         link: "https://naftalib.github.io/MemeGenerator/" 
     },
     four:{
-        img:"./assets/DATA/imgs/TodoList.PNG",
+        img:"./assets/DATA/imgs/react.PNG",
         title: 'To do list',
         description: `This app was part of a course on React in which I learned and mastered core React concepts such as state manipulation, conditional rendering, props and componant styling`,
         code: 'https://github.com/naftalib/todoApp',
@@ -30,22 +30,22 @@ const projects = {
     
     },
     five:{
-        img:"./assets/DATA/imgs/GoogleKeep.JPG",
+        img:"./assets/DATA/imgs/js.PNG",
         title: 'Google Keep clone',
         description: `This project was the first of a JS boot camp which I completed. The aim was to mimic the Google Keep app's basic functionality of taking and storing notes. Check out my code on GitHub by clicking on the link below`,
 		code: 'https://github.com/naftalib/GoogleKeepClone',
 		link: 'https://naftalib.github.io/GoogleKeepClone/'
     },
     six:{
-        img:"./assets/DATA/imgs/HN.JPG",
-        title: 'News Feed',
+        img:"./assets/DATA/imgs/react.PNG",
+        title: 'Github finder',
         description: `I used this project as the playground to get familiar with React hooks in functional components. I coded it from scratch with hooks and then redid it using class-based components so as to fully grasp the mechanics of using the 'useState' and 'useEffect' hooks`,
-		code: 'https://github.com/naftalib/news-test',
-		link: 'https://naftalib.github.io/news-test/'
+		code: 'https://github.com/naftalib/github_finder',
+		link: 'https://github.com/naftalib/github_finder'
     
     },
     seven:{
-        img:"./assets/DATA/imgs/parking.PNG",
+        img:"./assets/DATA/imgs/js.PNG",
         title: 'Parking tracker',
         description: `I developped this App as part of a coding challenge that I expanded on. See the README file for more details.`,
 		code: 'https://github.com/naftalib/parking-tracker/blob/API/client/js/app.js',
