5aa8cf3235e300bc196bbe4bfe91676698bf785d
Naftali Breuning <45719650+naftalib@users.noreply.github.com>
2022-06-15 22:38:57 +0300

    Update projects.js

--- JS/projects.js
+++ JS/projects.js
@@ -1,13 +1,15 @@
+//Obj containing an object with each project's details
+
 const projects = {
     one:{
-        img: "./assets/DATA/imgs/js.PNG",
+        img: "./assets/DATA/imgs/js.png",
         title: 'Personal Dashboard app',
         description: `This little app is a cool chrome extention based off of the momentum browser extention. I gave it a few other features like a currency converter.`,	
         code: 'https://github.com/naftalib/dashBoard/blob/main/index.js',
         link: 'https://github.com/naftalib/dashBoard'
     },
     two:{
-        img:"./assets/DATA/imgs/react.PNG",
+        img:"./assets/DATA/imgs/react.png",
         title: 'Robo Cards',
         description: `This React app displays character cards, generated with a http-call to robohash.org to retrieve character figures and the JSON placeholder/users API. The characters are searchable according to name. I plan on adding some more features to it soon`,
 		code: 'https://github.com/naftalib/roboQuotes/tree/main',
@@ -22,7 +24,7 @@ const projects = {
         link: "https://naftalib.github.io/MemeGenerator/" 
     },
     four:{
-        img:"./assets/DATA/imgs/react.PNG",
+        img:"./assets/DATA/imgs/react.png",
         title: 'To do list',
         description: `This app was part of a course on React in which I learned and mastered core React concepts such as state manipulation, conditional rendering, props and componant styling`,
         code: 'https://github.com/naftalib/todoApp',
@@ -30,14 +32,14 @@ const projects = {
     
     },
     five:{
-        img:"./assets/DATA/imgs/js.PNG",
+        img:"./assets/DATA/imgs/js.png",
         title: 'Google Keep clone',
         description: `This project was the first of a JS boot camp which I completed. The aim was to mimic the Google Keep app's basic functionality of taking and storing notes. Check out my code on GitHub by clicking on the link below`,
 		code: 'https://github.com/naftalib/GoogleKeepClone',
 		link: 'https://naftalib.github.io/GoogleKeepClone/'
     },
     six:{
-        img:"./assets/DATA/imgs/react.PNG",
+        img:"./assets/DATA/imgs/react.png",
         title: 'Github finder',
         description: `I used this project as the playground to get familiar with React hooks in functional components. I coded it from scratch with hooks and then redid it using class-based components so as to fully grasp the mechanics of using the 'useState' and 'useEffect' hooks`,
 		code: 'https://github.com/naftalib/github_finder',
@@ -45,7 +47,7 @@ const projects = {
     
     },
     seven:{
-        img:"./assets/DATA/imgs/js.PNG",
+        img:"./assets/DATA/imgs/js.png",
         title: 'Parking tracker',
         description: `I developped this App as part of a coding challenge that I expanded on. See the README file for more details.`,
 		code: 'https://github.com/naftalib/parking-tracker/blob/API/client/js/app.js',
@@ -54,7 +56,7 @@ const projects = {
     }
   }
   
-  
+ //Fn taked in the projects Obj as arg and returns template string- HTML card
   function test(data){
   return `
 
@@ -69,11 +71,11 @@ const projects = {
         </ul> 
         </div>
   </div>
-
-  
   `
   }
   
+//Instantiation of each project - deployed to the DOM
+
   document.querySelector("#project-1").innerHTML = `${test(projects.one)}`
   document.querySelector("#project-2").innerHTML = `${test(projects.seven)}`
   document.querySelector("#project-3").innerHTML = `${test(projects.two)}`
